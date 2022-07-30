5e15f086f88e5808a14943dfa5f81a950e3ac5a0
Gihan-Madhusankha <gmadusanka1234@gmail.com>
2022-07-31 00:30:39 +0530

    Create server socket and naming the port

---
+++ INP-terminal_chat/Module_02/src/ServerApp.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * @author : Gihan Madhusankha
+ * 2022-07-31 12:15 AM
+ **/
+
+public class ServerApp {
+    public static void main(String[] args) {
+
+        final int PORT = 8000;
+        try {
+            ServerSocket serverSocket = new ServerSocket(PORT);
+            System.out.println("Server is running in the port "+ PORT);
+
+            Socket localSocket = serverSocket.accept();
+            System.out.println("Server accepted...");
+            System.out.println(localSocket.getPort());
+            System.out.println(localSocket.getInetAddress());
+
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
