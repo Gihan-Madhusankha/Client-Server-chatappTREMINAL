5e6c19a73d9143721a8d031a96d7f30ab18716a4
Gihan-Madhusankha <gmadusanka1234@gmail.com>
2022-07-30 23:00:16 +0530

    Create serversocket and naming the port

---
+++ INP-terminal_chat/Module_01/src/ServerApp.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+
+/**
+ * @author : Gihan Madhusankha
+ * 2022-07-30 10:57 PM
+ **/
+
+public class ServerApp {
+    public static void main(String[] args) {
+
+        final int PORT = 8000;
+        try {
+            ServerSocket serverSocket = new ServerSocket();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
